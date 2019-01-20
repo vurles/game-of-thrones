33c4c2dbd545f9418f1e6ade1896ff6c7ceadd66
vurles <vurles27@gmail.com>
2019-01-20 16:34:25 -0300

    jugando con la data

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,13 @@
  * Acá va solo la lógica
  * 
  */
+const data = DATA.characters;
+//console.log(data);
+
+const persoGOT = (data) => {
+    let personajes =[];
+    for(i = 0; i < data.length; i++){
+        personajes.push(data[i].characterName)
+    }
+    return personajes
+}
