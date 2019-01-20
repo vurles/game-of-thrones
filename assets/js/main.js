506b894d4a51a03f6dac89331c617bca486279e6
vurles <vurles27@gmail.com>
2019-01-20 16:31:51 -0300

    evento click del botón para mostrar nombres

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,13 @@
  * Acá va solo el manejo del DOM
  * 
  */
+const perso = persoGOT(data);
+const showPer = document.getElementById('btn');
+const boxPer = document.getElementById('showMe');
+
+
+showPer.addEventListener('click',() => {
+    for(let i = 0;i < perso.length; i++){
+        boxPer.innerHTML += '<p>'+perso[i]+'</p>'
+    }
+})
