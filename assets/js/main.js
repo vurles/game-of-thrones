d3201451d94b7e67806b3369b96a01fbec93892b
vurles <vurles27@gmail.com>
2019-01-20 18:13:29 -0300

    se muestran tarjetas con el nombre de cada personaje y su foto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,30 @@
-/**
- * 
- * Acá va solo el manejo del DOM
- * 
- */
-const perso = persoGOT(data);
-const showPer = document.getElementById('btn');
+const data = (DATA.characters);
+//const perso = persoGOT(data);
+//const showPer = document.getElementById('btn');
 const boxPer = document.getElementById('showMe');
 
-
-showPer.addEventListener('click',() => {
+//repaso
+/*showPer.addEventListener('click',() => {
     for(let i = 0;i < perso.length; i++){
         boxPer.innerHTML += '<p>'+perso[i]+'</p>'
     }
-})
+})*/
+//ejemplo de Ale para mostra, implementando forEach
+
+const showPer = (data)=>{
+let result = '';
+data.forEach(element => {
+    result = boxPer.innerHTML += `
+        <div class="card">
+        <div class="box">
+            <h2>${element.characterName}</h2>
+            <div class="img">
+            <img src=${element.characterImageFull}>
+            </div>
+        </div>
+        </div>` 
+ } 
+);
+return result;
+}
+window.load = showPer(data);
