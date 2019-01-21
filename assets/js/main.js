0dbd4e9822bb1b2fd1129d86c83c45d17650e50f
vurles <vurles27@gmail.com>
2019-01-21 01:58:25 -0300

    fatal/no me resulta la mostrar los capitulos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
-const data = (DATA.characters);
+const data = DATA.characters;// con esto veo la data de los personajes
+const datos = EPISODES.episodes;// accedo a la data de los episodios.
 //const perso = persoGOT(data);
 //const showPer = document.getElementById('btn');
-const boxPer = document.getElementById('showMe');
+const boxPer = document.getElementById('showMe');//contenedor de personajes
+const boxCap = document.getElementById('showTem')//contenedor de temporadas
 
 //repaso
 /*showPer.addEventListener('click',() => {
@@ -26,5 +28,22 @@ data.forEach(element => {
  } 
 );
 return result;
+};
+window.showPer = showPer(data);
+
+const showTemporada = (datos)=>{
+    let resultado = '';
+    datos.forEach(element => {
+        resultado = boxCap.innerHTML += `
+            <div class="video">
+                <h5>Titulo ${element.episodeTitle}</h5>
+                <div "${element.episodeLink}"></div>
+                <p>Este episodio se trata de : ${element.episodeDescription}</p>
+                <p></p>
+            </div>` 
+} 
+);
+return resultado;
 }
-window.load = showPer(data);
+
+window.showTemporada = showTemporada(datos);
